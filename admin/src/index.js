9587f7090f1c8846f6d3bf16553e5dd5e6760f33
Tom Barton <tbarty95@gmail.com>
2022-08-04 20:57:11 +0100

    Fixed depracated package refs and corresponding code

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,28 +1,31 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const config = require("config")
-const request = require("request")
+const express = require("express");
+const bodyParser = require("body-parser");
+const config = require("config");
+// Not happy with this workaround but the alternative is to completely re-write with ES6 imports
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json({limit: "10mb"}))
+app.use(bodyParser.json({ limit: "10mb" }));
 
 app.get("/investments/:id", (req, res) => {
-  const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
-    }
-  })
-})
+  const { id } = req.params;
+  fetch(`${config.investmentsServiceUrl}/investments/${id}`)
+    .then((response) => response.json())
+    .then((body) => {
+      res.send(body);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
 
 app.listen(config.port, (err) => {
   if (err) {
-    console.error("Error occurred starting the server", err)
-    process.exit(1)
+    console.error("Error occurred starting the server", err);
+    process.exit(1);
   }
-  console.log(`Server running on port ${config.port}`)
-})
+  console.log(`Server running on port ${config.port}`);
+});
